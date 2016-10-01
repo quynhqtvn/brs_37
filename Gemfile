086fb062fef15179a4ceb3a85633237c3461e0df
Quynh Thai <thaiquynh.inbox@gmail.com>
2016-10-01 23:03:10 +0700

    user signup

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem "coffee-rails", "~> 4.2"
 gem "jquery-rails"
 gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
+gem "will_paginate", "3.1.0"
+gem "bootstrap-will_paginate", "0.0.10"
 
 group :development, :test do
   gem "byebug", platform: :mri
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,27 @@
 class User < ApplicationRecord
+  before_save :downcase_email
+
   has_many :comments
   has_many :requests
   has_many :book_users
   has_many :books, through: :book_users
-  has_many :active_relationships, class_name: Relationship.name, 
-    foreign_key: "follower_id", dependent: :destroy
-  has_many :passive_relationships, class_name: Relationship.name, 
-    foreign_key: "followed_id", dependent: :destroy 
+  has_many :active_relationships, class_name: Relationship.name,
+    foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_relationships, class_name: Relationship.name,
+    foreign_key: :followed_id, dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
 end
