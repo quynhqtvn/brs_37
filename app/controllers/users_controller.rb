9533547b373ac91bd89bd83e7fde333060c64170
Quynh Thai <thaiquynh.inbox@gmail.com>
2016-10-03 17:18:10 +0700

    user login-logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: :index
+  before_action :load_user, only: [:show, :edit, :update]
+  before_action :verify_user, only: [:edit, :update, :show]
+
+  def index
+    @users = User.recent.paginate page: params[:page],
+      per_page: Settings.per_page
+  end
+
+  def show
+  end
 
   def new
     @user = User.new
@@ -14,9 +25,50 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = t "profile_updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation, :image
+  end
+
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t "please_login"
+      redirect_to login_url
+    end
+  end
+
+  def verify_user
+    @user = User.find(params[:id])
+    if @user.nil?
+      flash[:notice] = t "user_not_found"
+      redirect_to root_path
+    end
+    redirect_to root_path unless @user == current_user
+  end
+
+  def verify_admin
+    redirect_to root_path unless current_user.admin?
+  end
+
+  def load_user
+    @user = User.find_by params[:id]
+    if @user.nil?
+      flash[:notice] = t "user_not_found"
+      redirect_to root_path
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save :downcase_email
+  before_save {email.downcase!}
 
   has_many :comments
   has_many :requests
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  attr_accessor :remember_token
+
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
@@ -20,8 +22,36 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
-  private
-  def downcase_email
-    self.email = email.downcase
+  scope :recent, ->{order created_at: :desc}
+
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+        BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def downcase_email
+      self.email = email.downcase
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send "#{attribute}_digest"
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
   end
 end
