0226f5b742e89a54626fe362955cf145eb3cf54b
Quynh Thai <thaiquynh.inbox@gmail.com>
2016-10-05 16:35:41 +0700

    Admin static pages

--- app/models/concerns/database_query.rb
+++ app/models/concerns/database_query.rb
@@ -2,4 +2,8 @@ module DatabaseQuery
   def latest
     order created_at: :desc
   end
+
+  def sort_by_name
+    order name: :asc
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "static_pages#index"
-  resources :users
-  resources :books
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   get "/login", to: "sessions#new"
@@ -11,6 +9,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root "categories#index", as: "root"
     resources :users, only: :delete
-    resources :categories, only: [:new, :create]
+    resources :categories
   end
+
+  resources :users
+  resources :books
 end
